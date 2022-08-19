fe2762572cda5b3dda16b18d3bbe79a0eb3c7f1a
SHEFDAN <110844468+SHEFDAN@users.noreply.github.com>
2022-08-19 08:36:54 -0700

    Add files via upload

---
+++ ORDER ASCENDING AND DESCENDING/ORDER ASCENDING AND DESCENDING/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ORDER_ASCENDING_AND_DESCENDING
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("ascending order");
+
+            //declaring and initialing the array
+
+            int[] arr = { 3, 56, 5, 6, 99, 45, 10, 7, 1, 63, 12 };
+
+            //sort array in ascending order
+            Array.Sort(arr);
+            foreach (int i in arr)
+                Console.WriteLine(i);
+            Console.WriteLine("descending order");
+
+
+            // reverse array
+            Array.Reverse(arr);
+            foreach (int value in arr)
+            {
+
+                Console.WriteLine(value + " ");
+            }
+
+        }
+    }
+}
